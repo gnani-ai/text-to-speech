f29b69196b1683b3bcdf2c1b2e0ead7f6c72c752
chaithrags <chaithrags@gnani.ai>
2022-06-16 08:34:10 +0530

    this release supports only python3.6

--- rest-codes/Python-Client/README.md
+++ rest-codes/Python-Client/README.md
@@ -2,13 +2,11 @@
 
 ### Note 
 - The tts rest services are for restricted users only. If you wish to opt for tts services , please contact operations@gnani.ai
-- You must have minimum python version 3.8 installed to this project.
-- 
+
 ## Installation
 
 ### Ubuntu
-- You can install python3.8 or higher versions
-- apt-get install python3.8
+- apt-get install python3.6
 - apt-get install python3-pip
 
 ### Mac OSX
@@ -17,6 +15,7 @@
 ___
 ### How to run Applicatiton
 - update details in `user.config` and add certificate file in root directory.
-- `pip3 install -r requirements.txt`
+- `pip3 install requests`
+- `pip3 install pytz`
 - `python3 tts-client.py` 
-- check the audiofile with the given input text
+- check the json response with audioContent
--- rest-codes/Python-Client/tts-client.py
+++ rest-codes/Python-Client/tts-client.py
@@ -1,8 +1,5 @@
 import json
-import httpx
-import numpy as np
-from scipy.io.wavfile import write
-
+import requests
 from configparser import ConfigParser
 from log_config.logger import get_logger
 
@@ -12,70 +9,61 @@ logger = get_logger(__name__)
 parser = ConfigParser()
 parser.read('user.config')
 
-LANGUAGE_MAPPING = {'en-IN': "english_female",
-                    'kn-IN': "kannada_female",
-                    'hi-IN': "hindi_female",
-                    'ta-IN': "tamil_female",
-                    'mr-IN': "marathi_female"}
 
-
-def tts_service(request_headers, request_payload, lang_code, sample_rate):
+def tts_service(request_headers, request_payload):
     """ Method to send text and get converted audio string in response
         Args:
             request_headers (json): request headers
             request_payload (json): request payload
-            lang_code (str) : string
         Raises:
             Exceptions
         Returns:
             response: Response from the server
     """
     try:
-        output_audio_file_name = "tts_output.wav"
-        api_url = "https://asr.gnani.ai/predictions/" + str(LANGUAGE_MAPPING[lang_code])
-        response = httpx.post(api_url, data=request_payload, headers=request_headers)
-        response_json = response.json()
-        audio = response_json["audio_raw"]
-        audio = np.asarray(audio, dtype=np.int16)
-        write(output_audio_file_name, int(sample_rate), audio)
-        logger.info(f"!!!! Successfully generated audio file = {output_audio_file_name} with the given samplerate !!!!!")
-    except Exception as exception:
-        logger.exception('Exception in getting audio from TTS response - !' + str(exception))
-        logger.info(f"Actual Response from tts server - {response.text}")
+        response = requests.request("GET", api_url,verify=cert,
+                                    headers=request_headers,
+                                    data=json.dumps(request_payload))
+
+        return response.json()
+
+    except BaseException as exception:
+        logger.exception('Exception in getting tts !' + str(exception))
+        return {"audioContent": ""}
 
 
 if __name__ == '__main__':
     """ Set all below parameters in the config file. """
 
     logger.info("Main Method ! - Start")
+    api_url = parser.get('TTS_USER', 'TTS_API_URL')
     token = parser.get('TTS_USER', 'TOKEN')
     accesskey = parser.get('TTS_USER', 'ACCESSKEY')
     language = parser.get('TTS_USER', 'LANGUAGE')
     product = parser.get('TTS_USER', 'PRODUCT')
-    lang_code = parser.get('TTS_USER', 'LANGUAGE')
 
     text = parser.get('TTS_REQUEST_BODY', 'TEXT')
+    lang_code = parser.get('TTS_REQUEST_BODY', 'LANGCODE')
+    name = parser.get('TTS_REQUEST_BODY', 'NAME')
+    ssml_gender = parser.get('TTS_REQUEST_BODY', 'SSMLGENDER')
+    audio_encoding = parser.get('TTS_REQUEST_BODY', 'AUDIOENCODING')
     sample_rate = parser.get('TTS_REQUEST_BODY', 'SAMPLERATE')
-    alpha = parser.get('TTS_REQUEST_BODY', 'ALPHA')
-    pitch = parser.get('TTS_REQUEST_BODY', 'PITCH')
-    pauses = parser.get('TTS_REQUEST_BODY', 'PAUSES')
 
     """SSL Configuration goes here.
     Paste the 'chain.pem' mailed to you in the root directory.
     """
     cert = "cert.pem"
-
+    
     # construct request headers
-    headers = {"token": token, "accesskey": accesskey,
-               "product": product, "Content-Type": "application/x-www-form-urlencoded"}
+    headers = {"token": token, "accesskey": accesskey, "lang": language,
+               "product": product,"Content-Type": "application/json"}
 
     # construct response headers
-    payload = {"input": text,
-               "alpha": alpha,
-               "pitch": pitch,
-               "pauses": pauses,
-               "sampleRate": sample_rate}
+    payload = {"input": {"text": text},
+               "voice": {"languageCode": lang_code, "name": name,
+                         "ssmlGender": ssml_gender},
+               "audioConfig": {"audioEncoding": audio_encoding,"sampleRate":sample_rate}}
 
-    tts_service(headers, payload, lang_code, sample_rate)
+    response = tts_service(headers, payload)
+    logger.info("Response from tts server : {}".format(response))
     logger.info("Main Method ! - End")
-
